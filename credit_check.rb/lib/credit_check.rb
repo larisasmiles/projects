c222f0df2e49d1d4f5a4ee6cb7f545c45b644e2c
Larisasmiles <larisasmiles@gmail.com>
2017-05-07 18:28:42 -0600

    initial commit

---
+++ credit_check.rb/lib/credit_check.rb
@@ -0,0 +1,49 @@
+# require "pry"
+class CreditCheck
+  attr_reader :card
+
+  def initialize(card)
+    @card = card
+  end
+
+  def card_reverse(card)
+    card.to_s.split("").reverse
+  end
+
+  def every_other_num_double(array)
+    array.map.with_index do |num, idx|
+     idx.odd? ? (num.to_i * 2).to_s : num
+    end
+  end
+
+
+  def add_double_digits(array)
+    array.map.with_index do |num|
+      num.to_s.length > 1 ? num[0].to_i + num[1].to_i : num.to_i
+    end
+  end
+
+  def sum_of_all_digits
+    sum_total = []
+    double_nums.each do |num|
+      sum_total << num.to_i
+    end
+    sum_total.inject(:+)
+  end
+
+  def validate_card
+    if sum_of_all_digits % 10 == 0
+      true
+    else
+      false
+    end
+  end
+
+  def credit_check_message
+    if validate_card == true
+      puts "Valid Credit Card Number!"
+    else
+      puts "Not Valid Credit Card Number!"
+    end
+  end
+end
---
+++ credit_check.rb/test/credit_check_test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/credit_check'
+require "pry"
+
+class CreditCheckTest < MiniTest::Test
+
+  def test_if_it_exists
+    card = CreditCheck.new(4539355764212759)
+    assert_instance_of CreditCheck, card
+    binding.pry
+  end
+
+  def test_if_card_splits
+    c = CreditCheck.new(4539355764212759)
+
+    assert_equal c.card_reverse(c.card), ["9", "5", "7", "2", "1", "2", "4", "6", "7", "5", "5",
+                                        "3", "9", "3", "5", "4"]
+  end
+
+  def test_if_every_other_num_doubles
+    c = CreditCheck.new(4539355764212759)
+    creverse = c.card_reverse(c.card)
+
+    assert_equal c.every_other_num_double(creverse), ["9", "10", "7", "4", "1", "4", "4", "12", "7", "10", "5",
+                                                      "6", "9", "6", "5", "8"]
+  end
+
+  def test_if_it_adds_double_digits
+    c = CreditCheck.new(4539355764212759)
+    creverse = c.card_reverse(c.card)
+    doubled = c.add_double_digits(creverse)
+
+    assert_equal c.add_double_digits(doubled), [9, 1, 7, 4, 1, 4, 4, 3, 7, 1, 5,
+                                                6, 9, 6, 5, 8]
+  end
+
+
+end
