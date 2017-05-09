179d6e04d3ce872fa29f6327362fdf6797d8fa15
Larisasmiles <larisasmiles@gmail.com>
2017-05-09 14:03:12 -0600

    tests passing

--- jungle_beat/test/node_test.rb
+++ jungle_beat/test/node_test.rb
@@ -7,19 +7,16 @@ class NodeTest < Minitest::Test
 
   def test_if_node_exists
     node = Node.new("plop")
-
     assert_instance_of Node, node
   end
 
   def test_if_node_plops
     node = Node.new("plop")
-
     assert_equal "plop", node.data
   end
 
   def test_if_next_node_is_nil
     node = Node.new("plop")
-
     assert_equal nil, node.next_node
   end
 end
