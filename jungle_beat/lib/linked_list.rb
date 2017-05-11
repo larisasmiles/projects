7fec22b8a1b940e7d20fe6ff87dd346f1255351c
Larisasmiles <larisasmiles@gmail.com>
2017-05-11 09:56:33 -0600

    update linked_list

--- jungle_beat/lib/linked_list.rb
+++ jungle_beat/lib/linked_list.rb
@@ -3,50 +3,50 @@ require './lib/node'
 class LinkedList
   attr_reader :head
 
-  def initialize
-    @head = nil
+  def initialize(head = nil)
+    @head = head
+    @string = ""
   end
 
   def append(data)
-    if head.nil?
+    current = @head
+    if head == nil
       @head = Node.new(data)
+      @head.data
     else
-      current = @head
-    until current.next_node.nil?
+      until current.next_node == nil
       current = current.next_node
-    end
+      end
       current.next_node = Node.new(data)
     end
     data
   end
 
   def count
-    if @head.nil?
-      counter = 0
-    else
-      counter = 1
-      current = @head
-      until current.next_node.nil?
-        current = current.next_node
-        counter += 1
-      end
-      counter
+    counter = 1
+    current = @head
+    until current.next_node == nil
+      counter += 1
+      current = current.next_node
     end
+    counter
   end
 
   def to_string
+    beats = " "
     current = @head
-    beats = []
-    beats << @head.data
-    until current.next_node.nil?
+    beats = current.data + ' '
+    until current.next_node == nil
       current = current.next_node
-      beats << current.data
+      beats = beats + current.data + ' '
     end
-    beats.join(" ")
+    beats.split(' ')
   end
 
-  def prepend(data)
-    current = Node.new(data)
-    @head = current
-  end
+  # def prepend(data)
+  #   current = Node.new(data)
+  #   current.next_node = @head
+  #   @head = current
+  # end
+  # data
 end
