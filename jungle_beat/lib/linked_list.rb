3101bc36c21477f6aea96dd09f74099c1d23e8e5
Larisasmiles <larisasmiles@gmail.com>
2017-05-10 20:15:37 -0600

    end of iteration 2

--- jungle_beat/lib/linked_list.rb
+++ jungle_beat/lib/linked_list.rb
@@ -1,11 +1,10 @@
-require_relative './lib/node'
+require './lib/node'
 
 class LinkedList
-  attr_accessor :head
+  attr_reader :head
 
   def initialize
     @head = nil
-    @string = ""
   end
 
   def append(data)
@@ -18,10 +17,11 @@ class LinkedList
     end
       current.next_node = Node.new(data)
     end
+    data
   end
 
   def count
-    if @head.nil
+    if @head.nil?
       counter = 0
     else
       counter = 1
@@ -30,17 +30,19 @@ class LinkedList
         current = current.next_node
         counter += 1
       end
+      counter
     end
-    counter
   end
 
 
   def to_string
     current = @head
     beats = []
+    beats << @head.data
     until current.next_node.nil?
       current = current.next_node
       beats << current.data
     end
+    beats.join(" ")
   end
 end
--- jungle_beat/test/linked_list_test.rb
+++ jungle_beat/test/linked_list_test.rb
@@ -1,48 +1,54 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/linked_list'
-require_relative './lib/node'
-
 require "pry"
 
 class LinkedListTest < Minitest::Test
 
-def test_if_linked_list_exists
+  def test_if_linked_list_exists
   list = LinkedList.new
-  assert_instance_of LinkedList, list
-end
+  assert list, LinkedList.new
+  end
 
-def test_if_head_equals_nil
+  def test_if_head_equals_nil
   list = LinkedList.new
+
   assert_equal nil, list.head
-end
+  end
 
-def test_if_it_appends
+  def test_if_it_appends
   list = LinkedList.new
+
   assert_equal "doop", list.append("doop")
-end
+  end
 
-def test_if_next_node_is_nil
+  def test_if_next_node_is_nil
   list = LinkedList.new
+
   assert_equal nil, list.head
-end
+  end
 
-def test_if_it_counts
+  def test_if_it_counts
   list = LinkedList.new
-  assert_equal 1, list.count
-end
+  list.append ("doop")
 
-def test_if_to_string_works
+  assert_equal 1, list.count
+  end
+
+  def test_if_to_string_works
   list = LinkedList.new
   list.append("doop")
-  assert_equal "doop", list.to_string
-end
-
-def test_if_it_appends_deep
-  list = LinkedList.new
   list.append("deep")
-  assert_equal "deep", list.append("deep")
 
-end
-binding.pry
+  assert_equal "doop deep", list.to_string
+  end
+
+  def test_if_it_appends
+  list = LinkedList.new
+  list.append("doop")
+  list.append("deep")
+
+  assert_equal "deep", list.head.next_node.data
+  binding.pry
+  end
 end
